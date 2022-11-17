a81e202e42363bc1e6be3a475a0dd18eeeaf3354
dilya <1dilnora777@gmail.com>
2022-11-17 10:57:22 +0500

    task001 added

---
+++ task001_41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел меньше 0 ввел пользователь.
+//1,-7,567, 89, 223 -> 3
+//0,7,8,-2,-2 -> 2
+
+Console.Write("Enter the numbers: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+int result = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < 0)
+    {
+        result++;
+    }
+}
+ 
+Console.WriteLine($"Quantity of elements less than 0: {result}");
