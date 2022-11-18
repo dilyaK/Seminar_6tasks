48f1dfb8c1f72de1fcc185ac0ca4776e1810af7c
dilya <1dilnora777@gmail.com>
2022-11-18 23:59:12 +0500

    task1 fixed

--- task001_41/Program.cs
+++ task001_41/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Пользователь вводит с клавиатуры М чисел.
 // Посчитайте, сколько чисел меньше 0 ввел пользователь.
-//1,-7,567, 89, 223 -> 3
+//1,-7,567, 89, 223 -> 1
 //0,7,8,-2,-2 -> 2
 
-Console.Write("Enter numbers using spaces: "); // fill the array using spaces
+Console.WriteLine("Enter numbers using spaces: "); // fill the array using spaces
 int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
 int result = 0;
  
